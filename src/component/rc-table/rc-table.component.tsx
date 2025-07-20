246cd9e2e6988704f28f20d2f4ef1519db2a0d0f
dalogsamurai <thelogsamurai@gmail.com>
2025-07-21 01:41:48 +0300

    feat: RCTable wip

---
+++ src/component/rc-table/rc-table.component.tsx
@@ -0,0 +1,40 @@
+import Table from "rc-table";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    width: 100,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+    width: 100,
+  },
+  {
+    dataIndex: "address",
+    title: "Address",
+    key: "address",
+    width: 200,
+  },
+  {
+    title: "Operations",
+    dataIndex: "",
+    key: "operations",
+    render: () => <a href="#">Delete</a>,
+  },
+];
+
+const data = [
+  { name: "Jack", age: 28, address: "some where", key: "1" },
+  { name: "Rose", age: 36, address: "some where", key: "2" },
+];
+
+// TODO
+const RCTable = () => {
+  return <Table columns={columns} data={data} />;
+};
+
+export default RCTable;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router";
+import TanstackTable from "./component/tanstack-table/tanstack-table.component";
+import RCTable from "./component/rc-table/rc-table.component";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tanstack-table" element={<TanstackTable />} />
+        <Route path="/rc-table" element={<RCTable />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
