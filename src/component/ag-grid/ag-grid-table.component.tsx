ad9d18923d0d99c98e8725259272cc063cede7ee
dalogsamurai <thelogsamurai@gmail.com>
2025-08-04 02:28:06 +0300

    feat: AgGridTable add

---
+++ src/component/ag-grid/ag-grid-table.component.tsx
@@ -0,0 +1,87 @@
+// React Grid Logic
+import { useEffect, useState } from "react";
+import {
+  AllCommunityModule,
+  ColDef,
+  ColGroupDef,
+  GridOptions,
+  ModuleRegistry,
+} from "ag-grid-community";
+// Core CSS
+import { AgGridReact } from "ag-grid-react";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+const generateRandomData = (itemsCount: number) => {
+  const makesList = ["Toyota", "Ford", "BMW", "Audi", "Honda"];
+  const modelsList = ["Camry", "Focus", "X5", "A4", "Civic"];
+  const colorsList = ["Red", "Blue", "Silver", "Black", "White"];
+
+  return Array.from({ length: itemsCount }, (_, i) => ({
+    id: i + 1,
+    make: makesList[Math.floor(Math.random() * makesList.length)],
+    model: modelsList[Math.floor(Math.random() * modelsList.length)],
+    price: Math.floor(Math.random() * 90000) + 10000,
+    year: Math.floor(Math.random() * 10) + 2015,
+    color: colorsList[Math.floor(Math.random() * colorsList.length)],
+    sold: Math.random() > 0.5,
+    electric: false,
+  }));
+};
+
+const AgGridTable = () => {
+  const [rowData, _] = useState(generateRandomData(100));
+
+  const [columnDefs] = useState<(ColDef | ColGroupDef)[]>([
+    {
+      headerName: "Make & Model",
+      children: [
+        {
+          field: "make",
+          sortable: true,
+          filter: true,
+        },
+        {
+          field: "model",
+          filter: "agTextColumnFilter",
+        },
+      ],
+    },
+    {
+      headerName: "Price & Year",
+      children: [
+        {
+          field: "price",
+          filter: "agNumberColumnFilter",
+          valueFormatter: (params) => `$${params.value.toLocaleString()}`,
+        },
+        {
+          field: "year",
+          filter: "agNumberColumnFilter",
+        },
+      ],
+    },
+    {
+      field: "color",
+      filter: "agSetColumnFilter",
+    },
+  ]);
+
+  const gridOptions: GridOptions = {
+    rowSelection: "multiple",
+    pagination: true,
+    paginationPageSize: 20,
+  };
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        gridOptions={gridOptions}
+      />
+    </div>
+  );
+};
+
+export default AgGridTable;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router";
 import TanstackTable from "./component/tanstack-table/tanstack-table.component";
 import RCTable from "./component/rc-table/rc-table.component";
+import AgGridTable from "./component/ag-grid/ag-grid-table.component";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,6 +20,7 @@ root.render(
         <Route path="/" element={<TanstackTable />} />
         <Route path="/tanstack-table" element={<TanstackTable />} />
         <Route path="/rc-table" element={<RCTable />} />
+        <Route path="/ag-grid-table" element={<AgGridTable />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
